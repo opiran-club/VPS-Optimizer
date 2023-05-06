4dfd1c6cf40c642750fc5278528b0805b16a1ab6
Opiran club <130220895+OPIran-CluB@users.noreply.github.com>
2023-05-06 16:39:35 +0330

    Update Dropbear.sh

--- Dropbear.sh
+++ Dropbear.sh
@@ -73,13 +73,3 @@ function input_nameserver() {
     systemctl restart systemd-resolved
   fi
 }
-
-# install dropbear
-apt-get -y install dropbear
-sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=444/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 444 -p 80"/g' /etc/default/dropbear
-echo "/bin/false" >> /etc/shells
-echo "/usr/sbin/nologin" >> /etc/shells
-service ssh restart
-service dropbear restart
