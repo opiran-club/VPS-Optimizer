4ef0160782262af75dfc99e84ef2884857282ec4
Opiran club <130220895+opiran-club@users.noreply.github.com>
2023-10-02 17:14:44 +0330

    Create stunnel.sh

---
+++ Install/stunnel.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#auto install debian Base
+#created by OPIran-Club
+
+# detaile name 
+country=ID
+state=opiran
+locality=opiran
+organization=OPIran Club
+organizationalunit=OPIran Club
+commonname=OPIran Club
+email=admin@OPIranClub.com
+
+# install stunnel
+apt-get install stunnel4 -y
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+[dropbear]
+accept = 443
+connect = 127.0.0.1:143
+[dropbear]
+accept = 222
+connect = 127.0.0.1:22
+[dropbear]
+accept = 444
+connect = 127.0.0.1:44
+[dropbear]
+accept = 777
+connect = 127.0.0.1:77
+END
+
+echo "=================  create Certificate OpenSSL ======================"
+echo "========================================================="
+# create Certificate
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
+# configure stunnel
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+service stunnel4 restart
+
+#happy auto install stunnel debian
+rm -rf stunnel.sh
